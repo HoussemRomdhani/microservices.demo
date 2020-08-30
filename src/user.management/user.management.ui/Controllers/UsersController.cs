c77fa3016f5b724d3b0d9cbb0986401bfe9976b0
Houssem.Romdhani <romdhanihoussem77@gmail.com>
2020-08-30 22:44:53 +0200

    add user management project

---
+++ src/user.management/user.management.ui/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace user.management.ui.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
